45eb88f7e759d3d2b592a40489da0c2cd69c594d
valiclud <valicek@atlas.cz>
2026-04-08 16:40:22 +0200

    First commit - skeleton.

---
+++ review-service/src/test/java/com/github/valiclud/old_languages/core/review/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.valiclud.old_languages.core.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
