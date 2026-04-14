a9509a4c27622bcb4fe65b52c8040c3b82100c97
valiclud <valicek@atlas.cz>
2026-04-14 15:17:58 +0200

    Review service setup.

--- microservices/review-service/src/main/java/com/github/valiclud/old_languages/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/com/github/valiclud/old_languages/core/review/ReviewServiceApplication.java
@@ -2,12 +2,14 @@ package com.github.valiclud.old_languages.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class Application {
+@ComponentScan("com.github.valiclud")
+public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ReviewServiceApplication.class, args);
 	}
 
 }
