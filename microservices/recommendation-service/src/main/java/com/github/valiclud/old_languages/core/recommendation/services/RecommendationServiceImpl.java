e2a91b9df41f1a6443e0e45d74ff08e03cec7b9a
valiclud <valicek@atlas.cz>
2026-04-18 11:11:26 +0200

    RecommendationService and ReviewService correction.

--- microservices/recommendation-service/src/main/java/com/github/valiclud/old_languages/core/recommendation/services/RecommendationServiceImpl.java
+++ microservices/recommendation-service/src/main/java/com/github/valiclud/old_languages/core/recommendation/services/RecommendationServiceImpl.java
@@ -22,7 +22,7 @@ public class RecommendationServiceImpl implements RecommendationService {
   }
 
   @Override
-  public List<Recommendation> getRecommendations(int productId) {
+  public List<Recommendation> getRecommendations(Long productId) {
 
     if (productId < 1) {
       throw new InvalidInputException("Invalid productId: " + productId);
