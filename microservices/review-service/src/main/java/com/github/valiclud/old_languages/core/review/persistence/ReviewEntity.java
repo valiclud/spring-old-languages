0172e8aa162981743ace81821c56ae0727963428
valiclud <valicek@atlas.cz>
2026-05-13 09:18:15 +0200

    Review service adding JPA.

---
+++ microservices/review-service/src/main/java/com/github/valiclud/old_languages/core/review/persistence/ReviewEntity.java
@@ -0,0 +1,97 @@
+package com.github.valiclud.old_languages.core.review.persistence;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reviews", indexes = { @Index(name = "reviews_unique_idx", unique = true, columnList = "productId,reviewId") })
+public class ReviewEntity {
+
+  @Id @GeneratedValue
+  private int id;
+
+  @Version
+  private int version;
+
+  private int productId;
+  private int reviewId;
+  private String author;
+  private String subject;
+  private String content;
+  private String serviceAddress;
+  
+  public ReviewEntity() {
+  }
+
+  public ReviewEntity(int productId, int reviewId, String author, String subject, String content) {
+    this.productId = productId;
+    this.reviewId = reviewId;
+    this.author = author;
+    this.subject = subject;
+    this.content = content;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getVersion() {
+    return version;
+  }
+
+  public void setVersion(int version) {
+    this.version = version;
+  }
+
+  public int getProductId() {
+    return productId;
+  }
+
+  public void setProductId(int productId) {
+    this.productId = productId;
+  }
+
+  public int getReviewId() {
+    return reviewId;
+  }
+
+  public void setReviewId(int reviewId) {
+    this.reviewId = reviewId;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+  
+  public String getServiceAddress() {
+	    return serviceAddress;
+	  }
+  
+  public void setServiceAddress(String serviceAddress) {
+	  this.serviceAddress = serviceAddress;
+  }
+
+}
--- microservices/review-service/src/main/java/com/github/valiclud/old_languages/core/review/services/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/com/github/valiclud/old_languages/core/review/services/ReviewServiceImpl.java
@@ -1,45 +1,73 @@
 package com.github.valiclud.old_languages.core.review.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.github.valiclud.api.composite.core.review.Review;
 import com.github.valiclud.api.composite.core.review.ReviewService;
 import com.github.valiclud.api.exceptions.InvalidInputException;
+import com.github.valiclud.old_languages.core.review.persistence.ReviewEntity;
+import com.github.valiclud.old_languages.core.review.persistence.ReviewRepository;
 import com.github.valiclud.util.http.ServiceUtil;
+import com.github.valiclud.old_languages.core.review.services.ReviewMapper;
 
 @RestController
 public class ReviewServiceImpl implements ReviewService {
 
   private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
 
+  private final ReviewRepository repository;
+  
+  private final ReviewMapper reviewMapper;
+
   private final ServiceUtil serviceUtil;
 
-  public ReviewServiceImpl(ServiceUtil serviceUtil) {
+  public ReviewServiceImpl(ReviewRepository repository, ReviewMapper reviewMapper, ServiceUtil serviceUtil) {
+    this.repository = repository;
+    this.reviewMapper = reviewMapper;
     this.serviceUtil = serviceUtil;
   }
+/*
+  @Override
+  public Review createReview(Review body) {
+    try {
+      ReviewEntity entity = mapper.apiToEntity(body);
+      ReviewEntity newEntity = repository.save(entity);
 
+      LOG.debug("createReview: created a review entity: {}/{}", body.getProductId(), body.getReviewId());
+      return mapper.entityToApi(newEntity);
+
+    } catch (DataIntegrityViolationException dive) {
+      throw new InvalidInputException("Duplicate key, Product Id: " + body.getProductId() + ", Review Id:" + body.getReviewId());
+    }
+  }
+*/
   @Override
   public List<Review> getReviews(int productId) {
 
     if (productId < 1) {
       throw new InvalidInputException("Invalid productId: " + productId);
     }
+    
+    List<ReviewEntity> entityList = repository.findByProductId(productId);
+    List<Review> list = this.reviewMapper.entityListToApiList(entityList);
+    list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 
-    if (productId == 213) {
-      LOG.debug("No reviews found for productId: {}", productId);
-      return new ArrayList<>();
-    }
-
-    List<Review> list = new ArrayList<>();
-    list.add(new Review(productId, 1, "Author 1", "Subject 1", "Content 1", serviceUtil.getServiceAddress()));
-    list.add(new Review(productId, 2, "Author 2", "Subject 2", "Content 2", serviceUtil.getServiceAddress()));
-    list.add(new Review(productId, 3, "Author 3", "Subject 3", "Content 3", serviceUtil.getServiceAddress()));
-
-    LOG.debug("/reviews response size: {}", list.size());
+    LOG.debug("getReviews: response size: {}", list.size());
 
     return list;
   }
+/*
+  @Override
+  public void deleteReviews(int productId) {
+    LOG.debug("deleteReviews: tries to delete reviews for the product with productId: {}", productId);
+    repository.deleteAll(repository.findByProductId(productId));
+  }
+  */
 }
