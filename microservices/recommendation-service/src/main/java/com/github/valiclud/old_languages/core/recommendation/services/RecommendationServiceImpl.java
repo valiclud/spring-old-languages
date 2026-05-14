f6b2ff797d745717c3b32b75442cbb379b5b4c7a
valiclud <valicek@atlas.cz>
2026-05-14 19:11:43 +0200

    Recommendation service post and delete implementation

--- microservices/recommendation-service/src/main/java/com/github/valiclud/old_languages/core/recommendation/services/RecommendationServiceImpl.java
+++ microservices/recommendation-service/src/main/java/com/github/valiclud/old_languages/core/recommendation/services/RecommendationServiceImpl.java
@@ -1,13 +1,15 @@
 package com.github.valiclud.old_languages.core.recommendation.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 import com.github.valiclud.api.composite.core.recommendation.Recommendation;
 import com.github.valiclud.api.composite.core.recommendation.RecommendationService;
 import com.github.valiclud.api.exceptions.InvalidInputException;
+import com.github.valiclud.old_languages.core.recommendation.persistence.RecommendationEntity;
+import com.github.valiclud.old_languages.core.recommendation.persistence.RecommendationRepository;
 import com.github.valiclud.util.http.ServiceUtil;
 
 @RestController
@@ -15,31 +17,51 @@ public class RecommendationServiceImpl implements RecommendationService {
 
   private static final Logger LOG = LoggerFactory.getLogger(RecommendationServiceImpl.class);
 
+  private final RecommendationRepository repository;
+
+  private final RecommendationMapper mapper;
+
   private final ServiceUtil serviceUtil;
 
-  public RecommendationServiceImpl(ServiceUtil serviceUtil) {
+  public RecommendationServiceImpl(RecommendationRepository repository, RecommendationMapper mapper, ServiceUtil serviceUtil) {
+    this.repository = repository;
+    this.mapper = mapper;
     this.serviceUtil = serviceUtil;
   }
 
   @Override
   public List<Recommendation> getRecommendations(int productId) {
 
-    if (productId < 1) {
-      throw new InvalidInputException("Invalid productId: " + productId);
-    }
+	    if (productId < 1) {
+	      throw new InvalidInputException("Invalid productId: " + productId);
+	    }
+	    
+	    List<RecommendationEntity> entityList = repository.findByProductId(productId);
+	    List<Recommendation> list = mapper.entityListToApiList(entityList);
+	    list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 
-    if (productId == 113) {
-      LOG.debug("No recommendations found for productId: {}", productId);
-      return new ArrayList<>();
-    }
+	    LOG.debug("getRecommendations: response size: {}", list.size());
 
-    List<Recommendation> list = new ArrayList<>();
-    list.add(new Recommendation(productId, 1, "Author 1", 1, "Content 1", serviceUtil.getServiceAddress()));
-    list.add(new Recommendation(productId, 2, "Author 2", 2, "Content 2", serviceUtil.getServiceAddress()));
-    list.add(new Recommendation(productId, 3, "Author 3", 3, "Content 3", serviceUtil.getServiceAddress()));
-
-    LOG.debug("/recommendation response size: {}", list.size());
-
-    return list;
+	    return list;
   }
+
+  @Override
+  public Recommendation createRecommendation(Recommendation body) {
+	  try {
+	      RecommendationEntity entity = mapper.apiToEntity(body);
+	      RecommendationEntity newEntity = repository.save(entity);
+
+	      LOG.debug("createRecommendation: created a recommendation entity: {}/{}", body.getProductId(), body.getRecommendationId());
+	      return mapper.entityToApi(newEntity);
+
+	    } catch (DuplicateKeyException dke) {
+	      throw new InvalidInputException("Duplicate key, Product Id: " + body.getProductId() + ", Recommendation Id:" + body.getRecommendationId());
+	    }
+  }
+
+  @Override
+  public void deleteRecommendations(int productId) {
+	  LOG.debug("deleteRecommendations: tries to delete recommendations for the product with productId: {}", productId);
+	    repository.deleteAll(repository.findByProductId(productId));
+	  }
 }
