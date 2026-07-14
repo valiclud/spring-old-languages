bb09f95a065e8c567617d2f13c58f1de552d5de8
valiclud <valicek@atlas.cz>
2026-07-14 17:47:12 +0200

    Blocking react for ReviewServiceApplication

---
+++ microservices/review-service/src/main/java/com/github/valiclud/old_languages/core/review/services/MessageProcessorConfig.java
@@ -0,0 +1,53 @@
+package com.github.valiclud.old_languages.core.review.services;
+
+import java.util.function.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.valiclud.api.composite.core.review.Review;
+import com.github.valiclud.api.composite.core.review.ReviewService;
+import com.github.valiclud.api.event.Event;
+import com.github.valiclud.api.exceptions.EventProcessingException;
+
+@Configuration
+public class MessageProcessorConfig {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MessageProcessorConfig.class);
+
+  private final ReviewService reviewService;
+
+  public MessageProcessorConfig(ReviewService reviewService) {
+    this.reviewService = reviewService;
+  }
+
+  @Bean
+  public Consumer<Event<Integer, Review>> messageProcessor() {
+    return event -> {
+      LOG.info("Process message created at {}...", event.getEventCreatedAt());
+
+      switch (event.getEventType()) {
+
+        case CREATE:
+          Review review = event.getData();
+          LOG.info("Create review with ID: {}/{}", review.getProductId(), review.getReviewId());
+          reviewService.createReview(review).block();
+          break;
+
+        case DELETE:
+          int productId = event.getKey();
+          LOG.info("Delete reviews with ProductID: {}", productId);
+          reviewService.deleteReviews(productId).block();
+          break;
+
+        default:
+          String errorMessage = "Incorrect event type: " + event.getEventType() + ", expected a CREATE or DELETE event";
+          LOG.warn(errorMessage);
+          throw new EventProcessingException(errorMessage);
+      }
+
+      LOG.info("Message processing done!");
+    };
+  }
+}
